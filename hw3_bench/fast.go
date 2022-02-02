cb0cbd060c13e5844376fbda2a524bd8d8593553
BeautifuLie <lafazan.denis@gmail.com>
2022-02-02 17:47:29 +0200

    upd

--- hw3_bench/fast.go
+++ hw3_bench/fast.go
@@ -6,8 +6,8 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
-	"io/ioutil"
 	"os"
+	"runtime"
 	"strings"
 )
 
@@ -19,48 +19,29 @@ type User struct {
 
 // вам надо написать более быструю оптимальную этой функции
 func FastSearch(out io.Writer) {
+	runtime.GOMAXPROCS(8)
 	file, err := os.Open(filePath)
 	if err != nil {
 		panic(err)
 	}
 	defer file.Close()
-	// j, err := ioutil.ReadFile("./data/users.txt")
-	// if err != nil {
-	// 	fmt.Println("Error reading file", err)
-	// }
-	// var lines []string
-	// scanner := bufio.NewScanner(file)
-	// for scanner.Scan() {
-	// 	lines = append(lines, scanner.Text())
-	// }
-	// fileContents, err := ioutil.ReadAll(file)
-	// if err != nil {
-	// 	panic(err)
-	// }
+
 	in := bufio.NewScanner(file)
 	seenBrowsers := []string{}
 	uniqueBrowsers := 0
 	foundUsers := ""
 
-	// users := []User{}
+	for i := 0; in.Scan(); i++ {
 
-	// for _, line := range lines {
-	// 	user := User{}
-	// 	err = json.Unmarshal([]byte(line), &user)
-	// 	if err != nil {
-	// 		panic(err)
-	// 	}
-	// 	users = append(users, user)
-	// }
-	i := 0
-	for in.Scan() {
-		i++
 		row := in.Bytes()
-		if bytes.Contains(row, []byte("Android")) == false && bytes.Contains(row, []byte("MSIE")) == false {
+		if !bytes.Contains(row, []byte("Android")) && !bytes.Contains(row, []byte("MSIE")) {
 			continue
 		}
 		user := User{}
-		json.Unmarshal(row, &user)
+		err = json.Unmarshal(row, &user)
+		if err != nil {
+			return
+		}
 		isAndroid := false
 		isMSIE := false
 
@@ -109,14 +90,9 @@ func FastSearch(out io.Writer) {
 
 		// log.Println("Android and MSIE user:", user["name"], user["email"])
 		email := strings.Replace(user.Email, "@", " [at] ", -1)
-		foundUsers += fmt.Sprintf("[%d] %s <%s>\n", i-1, user.Name, email)
+		foundUsers += fmt.Sprintf("[%d] %s <%s>\n", i, user.Name, email)
 	}
 
 	fmt.Fprintln(out, "found users:\n"+foundUsers)
 	fmt.Fprintln(out, "Total unique browsers", len(seenBrowsers))
-}
-func main() {
-
-	FastSearch(ioutil.Discard)
-
 }
